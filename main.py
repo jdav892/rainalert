b337401f1f5a36c2c5fe6aac37bc38554d39e7b2
jdav892 <jadavila9@gmail.com>
2024-07-16 13:49:22 -0400

    json

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "75f90eaf7e859511c0d5dba3dadeb97d"
+
+weather_params = {
+    "lat": 28.9005,
+    "lon": 81.2637,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
